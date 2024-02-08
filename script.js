e2eaff754b3f6d12ffe68a94de7553ce066e15dd
Rayhona <rayhonamirzoyeva.ru>
2024-02-08 15:50:10 +0500

    kke

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-let h = document.querySelector('h1')
-let d = document.querySelector('div')
+// let h = document.querySelector('h1')
+// let d = document.querySelector('div')
 
 
-// h.onclick = (event)=>{
-//     console.log(event);
-//     if(event.shiftKey){
-//         h.style.color = 'red'
-//     }else {
-//         h.style.color = 'green'
-//     }
+// // h.onclick = (event)=>{
+// //     console.log(event);
+// //     if(event.shiftKey){
+// //         h.style.color = 'red'
+// //     }else {
+// //         h.style.color = 'green'
+// //     }
+// // }
+// // d.onclick = (e) => {
+// //     e.target.style.color = 'red'
+// // }
+
+// // LISTENERS
+// // ATTRIBUTE, ONTYPE, addEventListener()
+// function test (){
+//     console.log('ON ATTRIBUTE');
 // }
-// d.onclick = (e) => {
-//     e.target.style.color = 'red'
+
+// h.onclick = () => {
+//     console.log('ON TYPE');
+
 // }
 
-// LISTENERS
-// ATTRIBUTE, ONTYPE, addEventListener()
-function test (){
-    console.log('ON ATTRIBUTE');
+// h.onclick = () => {
+//     console.log('hvewjhcvhwy');
+// }
+
+// h.addEventListener('click',()=>{
+//     console.log('EVENT LISTENER');
+// })
+// h.addEventListener('click',()=>{
+//     console.log('EVENT LISTENER');
+// })
+
+
+// document.querySelector('#on').onclick = ()=>{
+//     document.querySelector('.prop').classList.add('rotate')
+// }
+// document.querySelector('#off').onclick = ()=>{
+//     document.querySelector('.prop').classList.remove('rotate')
+// }
+
+document.querySelector('.btn1').onclick = () =>{
+    document.querySelector('.container').style.backgroundColor = '#07022A'
+    document.querySelector('button').style.backgroundColor = '#E8DFFF',color = '#07022A'
+
 }
-
-h.onclick = () => {
-    console.log('ON TYPE');
-
-}
-
-h.onclick = () => {
-    console.log('hvewjhcvhwy');
-}
-
-h.addEventListener('click',()=>{
-    console.log('EVENT LISTENER');
-})
-h.addEventListener('click',()=>{
-    console.log('EVENT LISTENER');
-})
