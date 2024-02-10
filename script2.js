b8d57f20b12de612f3bfb27f70e9c4181316afa3
Rayhona <rayhonamirzoyeva.ru>
2024-02-10 21:39:57 +0500

    Add JavaScript

--- script2.js
+++ script2.js
@@ -1,21 +1,15 @@
 let imgE1 = document.querySelector('.image')
-let card1 = document.querySelector('.card1')
-let card2 = document.querySelector('.card2')
-let card3 = document.querySelector('.card3')
-let card4 = document.querySelector('.card4')
-let card5 = document.querySelector('.card5')
-card1.addEventListener('click',() =>{
-     imgE1.src = "/images/colors.jpg"
-})
-card2.addEventListener('click',() =>{
-    imgE1.src = "/images/1666206275_11-mykaleidoscope-ru-p-kartinka-na-zastavku-oboi-11.jpg"
-})
-card3.addEventListener('click',() =>{
-    imgE1.src = "/images/nature.jpg"
-})
-card4.addEventListener('click',() =>{
-    imgE1.src = "/images/tree.jpg"
-})
-card5.addEventListener('click',() =>{
-    imgE1.src = "/images/STAR.jpg"
-})
+let card = document.querySelectorAll('.card')
+
+// for (let i = 0; i < card.length; i++) {
+//     card[i].addEventListener('click', (e)=>{
+//         let imgUrl = e.target.getAttribute('src')
+//         imgE1.setAttribute('src', imgUrl)
+//     })
+// }
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener('click',(e)=>{
+        let imgUrl = e.target.getAttribute('src')
+        imgE1.setAttribute('src', imgUrl)
+  })
+}
